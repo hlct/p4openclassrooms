023172d14e41910d5a63787ea8e588abb18382aa
hlct <collet29000@gmail.com>
2020-07-13 17:22:50 +0200

    Modification

--- index.php
+++ index.php
@@ -124,6 +124,7 @@
 							$adminController -> supprflag();
 						}
 					}
+		
 					
 				}
 			    
--- views/FrontEnd/home.php
+++ views/FrontEnd/home.php
@@ -49,7 +49,6 @@
 						</div>
 					</div>
 				</div>
-				<br>
 			<?php endforeach; ?>
 			</div>
 	</section>
